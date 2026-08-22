2ea0ac71e96d7f46b0d6e27b843cd4fdba2bc9f9
amagus <amagus@users.noreply.github.com>
2026-08-22 14:13:42 +0000

    deploy: cfedbf4e3c0ae13bf5614f8dc14379b82107da8d

---
+++ OSXKeyState_8h.js
@@ -0,0 +1,13 @@
+var OSXKeyState_8h =
+[
+    [ "OSXKeyState", "classOSXKeyState.html", "classOSXKeyState" ],
+    [ "kCGKeyboardEventPrivateModifierFlags", "OSXKeyState_8h.html#a2793b7bac2bb80113c6d076a8cd88672", null ],
+    [ "kCgKeyboardIsLeftCmd", "OSXKeyState_8h.html#a10a5e42f4f28338bd52fc1eaa72965ee", null ],
+    [ "kCgKeyboardIsLeftControl", "OSXKeyState_8h.html#a928326efb82d22060b855cee55eabfc9", null ],
+    [ "kCgKeyboardIsLeftModKey", "OSXKeyState_8h.html#acdc1cb5c256f0d7f6c23c4214bd0b142", null ],
+    [ "kCgKeyboardIsLeftOption", "OSXKeyState_8h.html#a6c3496bf258755b3a87a6c75efa1239d", null ],
+    [ "kCgKeyboardIsRightCmd", "OSXKeyState_8h.html#a29be6ebd43f43843394cddb5cd29b2bd", null ],
+    [ "kCgKeyboardIsRightControl", "OSXKeyState_8h.html#af1f7d2d6dde2d5a8f14b8d29a5e282b4", null ],
+    [ "kCgKeyboardIsRightModKey", "OSXKeyState_8h.html#a68a3912b548891f4e1edffa68f6fd3f7", null ],
+    [ "kCgKeyboardIsRightOption", "OSXKeyState_8h.html#adf9a157540a736a0867267e8de0a4f7a", null ]
+];
